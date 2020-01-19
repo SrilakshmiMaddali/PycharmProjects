b9f32ee8bbe5090f536b70ff80bb20ffcf5da89a
Srilakshmi Maddali <srilakshmi.maddali@gmail.com>
2020-01-18 21:38:37 -0600

    website-blocker
    complete the website blocker app

--- app3 blocker/my-web-blocker.py
+++ app3 blocker/my-web-blocker.py
@@ -1,20 +1,28 @@
 import time
 from datetime import datetime as dt
 
-host_temp="hosts"
+host_temp="hosts.txt"
 host_path="/etc/hosts"
 redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,8)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,16):
+    if dt(dt.now().year,dt.now().month,dt.now().day,8)<dt.now()<dt(dt.now().year,dt.now().month,dt.now().day,21):
         print("Working hours..")
-        with open(host_temp,"r+") as file:
+        with open(host_path,"r+") as file:
             content=file.read()
             for website in website_list:
                 if website in content:
                     pass
                 else:
-                    file.write(redirect+"  "+website)
+                    file.write(redirect+"  "+website+"\n")
     else:
+        with open(host_path,"r+") as file:
+            content=file.readlines()
+            file.seek(0)
+            for line in content:
+                if not any(website in line for website in website_list):
+                    file.write(line)
+            file.truncate()
         print("Fun hours...")
+        time.sleep(5)
